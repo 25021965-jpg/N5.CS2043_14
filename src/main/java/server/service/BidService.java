deca74a2e53fb26755732074116cfed1b45bb3cd
MINH PHU <phu127744@gmail.com>
2026-05-04 10:57:24 +0700

    sửa exception

--- src/main/java/server/service/BidService.java
+++ src/main/java/server/service/BidService.java
@@ -1,6 +1,7 @@
 package server.service;
 
 import model.*;
+import server.exception.*;
 
 import java.math.BigDecimal;
 
@@ -9,21 +10,21 @@ public class BidService{
     public synchronized Bid placeBid(User user, Auction auction, BigDecimal amount) {
 
         if (user == null) {
-            throw new RuntimeException("User not logged in");
+            throw new AuthenticationException("User not logged in");
         }
 
         if (!user.hasRole(Role.BIDDER)) {
-            throw new RuntimeException("Not allowed to bid");
+            throw new InvalidBidException("Not allowed to bid");
         }
 
         if (auction.getStatus() != AuctionStatus.ACTIVE) {
-            throw new RuntimeException("Auction is closed");
+            throw new AuctionClosedException("Auction is closed");
         }
 
         BigDecimal minPrice = auction.getCurrentPrice().add(auction.getMinIncrement());
 
         if (amount.compareTo(minPrice) < 0) {
-            throw new RuntimeException("Bid must be >= " + minPrice);
+            throw new InvalidBidException("Bid must be >= " + minPrice);
         }
 
         Bid bid = new Bid(user, amount);
