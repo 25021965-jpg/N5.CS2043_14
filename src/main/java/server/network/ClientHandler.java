22b6e75ee72e2dbfb9e4bf95241c8c9d46a89c2e
MINH PHU <phu127744@gmail.com>
2026-05-07 22:58:31 +0700

    refactor nhỏ

--- src/main/java/server/network/ClientHandler.java
+++ src/main/java/server/network/ClientHandler.java
@@ -1,5 +1,7 @@
 package server.network;
 
+import server.dao.DatabaseService;
+
 import java.io.*;
 import java.net.Socket;
 
@@ -46,7 +48,7 @@ public class ClientHandler implements Runnable {
                 System.out.println("Đăng nhập - Username: " + username + ", Password: " + password);
 
                 // GỌI DATABASE ĐỂ KIỂM TRA
-                boolean success = client.network.DatabaseService.login(username, password);
+                boolean success = DatabaseService.login(username, password);
 
                 if (success) {
                     System.out.println("✓ Đăng nhập thành công: " + username);
@@ -74,7 +76,7 @@ public class ClientHandler implements Runnable {
 
                     System.out.println("Đăng ký - Username: " + username + ", Email: " + email);
 
-                    boolean success = client.network.DatabaseService.register(fullname, username, email, password);
+                    boolean success = DatabaseService.register(fullname, username, email, password);
 
                     if (success) {
                         System.out.println("✓ Đăng ký thành công: " + username);
