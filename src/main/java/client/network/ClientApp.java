26ce7897d5196922d17f51677e7a5ec428f74219
MINH PHU <phu127744@gmail.com>
2026-05-08 13:58:26 +0700

    tạo ClientSocket duy nhất

--- src/main/java/client/network/ClientApp.java
+++ src/main/java/client/network/ClientApp.java
@@ -1,16 +1,21 @@
 package client.network;
 
+import client.controller.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ClientApp extends Application {
+    private static ClientSocket socket;
     @Override
     public void start(Stage stage) throws Exception {
+        socket = new ClientSocket();
+
         FXMLLoader fxmlLoader =
                 new FXMLLoader(ClientApp.class.getResource("/fxml/login-view.fxml"));
 
+        LoginController ctrl = fxmlLoader.getController();
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("Auction System");
